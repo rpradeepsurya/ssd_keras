f6270d74e99b257e838dfea17b7cd78165781c7b
pradeepsurya <pradeepsurya@live.com>
2022-11-27 03:40:25 -0500

    Update keras_layer_AnchorBoxes.py

--- keras_layers/keras_layer_AnchorBoxes.py
+++ keras_layers/keras_layer_AnchorBoxes.py
@@ -18,9 +18,10 @@ limitations under the License.
 
 from __future__ import division
 import numpy as np
-import keras.backend as K
-from keras.engine.topology import InputSpec
-from keras.engine.topology import Layer
+import tensorflow as tf
+from tensorflow.keras import backend as K
+from tensorflow.keras.engine.topology import InputSpec
+from tensorflow.keras.engine.topology import Layer
 
 from bounding_box_utils.bounding_box_utils import convert_coordinates
 
